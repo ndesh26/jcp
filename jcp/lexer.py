ddb594709b045736b6ac678eac872051ccfec3fb
Rohith Mukku <rohithmukku@gmail.com>
2017-01-30 14:31:07 +0530

    Add a simple lexer for java
    Signed-off-by: Rohith Mukku <rohithmukku@gmail.com>

---
+++ jcp/lexer.py
@@ -0,0 +1,126 @@
+#-----------------------------------------------------------------------------------
+# lexer.py
+# Lexer for Java programming language in Python
+# Reference: https://docs.oracle.com/javase/specs/jls/se7/html/jls-18.html
+#-----------------------------------------------------------------------------------
+
+import ply.lex as lex
+
+# Reference: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/_keywords.html
+keywords = ('abstract', 'assert', 'boolean', 'break', 'byte'
+            'case', 'catch', 'char', 'class', 'const', 'continue',
+            'default', 'do', 'double', 'else', 'enum', 'extends',
+            'final', 'finally', 'float', 'for', 'goto', 'if',
+            'implements', 'import', 'instanceof', 'int', 'interface',
+            'long', 'native', 'new', 'package', 'private', 'protected',
+            'public', 'return', 'short', 'static', 'strictfp', 'super',
+            'switch', 'synchronized', 'this', 'throw', 'throws',
+            'transient', 'try', 'void', 'volatile', 'while')
+
+# Reference: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/operators.html
+# Tokens definitions
+tokens = [
+        'VARIABLE',
+        'NUMBER',
+        'CHARACTER',
+        'STRING',
+        'LINE_COMMENT', 'BLOCK_COMMENT',
+
+        'OR', 'AND', 'BIT_OR', 'BIT_AND',
+        'EQ', 'NE', 'GE', 'LE',
+
+        'PLUS', 'MINUS', 'LPAREN', 'RPAREN',
+        'TIMES', 'BY', 'GT', 'LT', 'REMAINDER',
+
+        'LSHIFT', 'RSHIFT', 'RRSHIFT',
+
+        'INCR', 'DECR',
+
+        'EQUALITY', 'NEQUALITY',
+
+        'PLUS_ASSIGN', 'MINUS_ASSIGN', 'TIMES_ASSIGN','BY_ASSIGN',
+        'REMAINDER_ASSIGN', 'LSHIFT_ASSIGN', 'RSHIFT_ASSIGN', 'RRSHIFT_ASSIGN',
+        'AND_ASSIGN', 'OR_ASSIGN', 'XOR_ASSIGN',
+
+        'TERNARY', 'XOR'
+        ] + [k.upper() for k in keywords]
+
+t_NUMBER        = r'\.?[0-9][0-9eE_1LdDa-fA-F.xXpP]'
+t_CHARACTER     = r'\'([^\\\n]|(\\.))*?\''
+t_STRING        = r'\"([^\\\n]|(\\.))*?\"'
+t_LINE_COMMENT  = '//.*'
+
+def t_BLOCK_COMMENT(t):
+    r'/\*(.|\n)*?\*/'
+    t.lexer.lineno += t.value.count('\n')
+
+# LOGICAL and BITWISE OPERATIONS
+t_OR        = r'\|\|'
+t_AND       = r'&&'
+t_BIT_OR    = r'\|'
+t_BIT_AND   = r'&'
+
+# COMPARISIONS
+t_GT    = '>'
+t_LT    = '<'
+t_EQ    = '='
+t_NE    = '!='
+t_GE    = '>='
+t_LE    = '<='
+
+# ARITHEMETIC OPERATIONS
+t_PLUS      = r'\+'
+t_MINUS     = r'\-'
+t_TIMES     = r'\*'
+t_BY        = r'/'
+t_REMAINDER = '%'
+
+# SHIFT
+t_LSHIFT    = '<<'
+t_RSHIFT    = '>>'
+t_RRSHIFT   = '>>>'
+
+# INCREMENT & DECREMENT
+t_INCR      = r'\+\+'
+t_DECR      = r'\+\+'
+
+# EQUALITY
+t_EQUALITY  = '=='
+
+# ASSIGNMENT
+t_PLUS_ASSIGN       = r'\+='
+t_MINUS_ASSIGN      = '-='
+t_TIMES_ASSIGN      = r'\*='
+t_BY_ASSIGN         = '/='
+t_REMAINDER_ASSIGN  = '%='
+t_LSHIFT_ASSIGN     = '<<='
+t_RSHIFT_ASSIGN     = '>>='
+t_RRSHIFT_ASSIGN    = '>>>='
+t_AND_ASSIGN        = '&='
+t_OR_ASSIGN         = r'\|='
+t_XOR_ASSIGN        = '\^='
+
+# Ignore indentation
+#t_IGNORE    = ' \t\f'
+
+# VARIABLES & NEWLINES
+def t_VARIABLE(t):
+    '[A-Za-z_$][A-Za-z0-9_$]*'
+    if t.value in keywords:
+        t.type = t.value.upper()
+    return t
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+def t_newline2(t):
+    r'\r\n'
+    t.lexer.lineno += len(t.value)/2
+
+def t_error(t):
+    print("Illegal character '{}' ({}) in line {}".format(t.value[0], hex(ord(t.value[0])), t.lexer.lineno))
+    t.lexer.skip(1)
+
+# Lexer build
+lexer = lex.lex()
