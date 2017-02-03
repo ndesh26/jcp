47dcbfe1a273da5593a72f5e3d9780d449daaa61
Nayan Deshmukh <nayan26deshmukh@gmail.com>
2017-02-03 22:05:00 +0530

    parser: add rules statements and blocks
    also generalise functions of ast according to number of child
    a node has
    
    Signed-off-by: Nayan Deshmukh <nayan26deshmukh@gmail.com>

--- jcp/ast.py
+++ jcp/ast.py
@@ -4,37 +4,53 @@ graph = pydot.Dot(graph_type='graph')
 
 k = 0
 
-def binary_op(op_label, node1_id, node2_id):
+def three_child_node(label, node1_id, node2_id, node3_id):
+    global k
+    node1 = pydot.Node(node1_id)
+    node2 = pydot.Node(node2_id)
+    node3 = pydot.Node(node3_id)
+    k+=1
+    parent_node = pydot.Node(k,label=label)
+
+    graph.add_node(parent_node)
+
+    graph.add_edge(pydot.Edge(parent_node, node1))
+    graph.add_edge(pydot.Edge(parent_node, node2))
+    graph.add_edge(pydot.Edge(parent_node, node3))
+
+    return k
+
+def two_child_node(label, node1_id, node2_id):
     global k
     node1 = pydot.Node(node1_id)
     node2 = pydot.Node(node2_id)
     k+=1
-    node_op = pydot.Node(k,label=op_label)
+    parent_node = pydot.Node(k,label=label)
 
-    graph.add_node(node_op)
+    graph.add_node(parent_node)
 
-    graph.add_edge(pydot.Edge(node_op, node1))
-    graph.add_edge(pydot.Edge(node_op, node2))
+    graph.add_edge(pydot.Edge(parent_node, node1))
+    graph.add_edge(pydot.Edge(parent_node, node2))
 
-    return k 
+    return k
 
-def single_node(label):
+def one_child_node(label, node_id):
+    global k
+    node = pydot.Node(node_id)
+    k+=1
+    parent_node = pydot.Node(k,label=label)
+
+    graph.add_node(parent_node)
+    graph.add_edge(pydot.Edge(parent_node, node))
+
+    return k
+
+def node_create(label):
     global k
     k+=1
     node = pydot.Node(k,label=label)
 
     graph.add_node(node)
-
-    return k
-
-def unary_op(op_label, node_id):
-    global k
-    node = pydot.Node(node_id)
-    k+=1
-    node_op = pydot.Node(k,label=op_label)
-
-    graph.add_node(node_op)
-    graph.add_edge(pydot.Edge(node_op, node))
 
     return k
 
--- jcp/parser_rules.py
+++ jcp/parser_rules.py
@@ -3,19 +3,126 @@ import ast
 
 from lexer import tokens
 
+# Statements
+def p_block(p):
+    '''block : LBRACE block_statements RBRACE'''
+    lbrace = ast.node_create(p[1])
+    rbrace = ast.node_create(p[3])
+    p[0] = ast.three_child_node("block", lbrace, p[2], rbrace)
+    ast.end()
+
+def p_block_statements(p):
+    '''block_statements : block_statement
+                        | block_statements block_statement'''
+    if len(p) == 2:
+        p[0] = ast.one_child_node("block_statements", p[1])
+    else:
+        p[0] = ast.two_child_node("block_statements", p[1], p[2])
+
+def p_block_statement(p):
+    '''block_statement : local_variable_declaration_statement
+                       | statement'''
+    p[0] = ast.one_child_node("block_statement", p[1])
+
+def p_local_variable_declaration_statement(p):
+    '''local_variable_declaration_statement : local_variable_declaration SEMICOLON'''
+    tmp = ast.node_create(p[2])
+    p[0] = ast.two_child_node("local_variable_declaration_statement", p[1], tmp)
+
+def p_local_variable_declaration(p):
+    '''local_variable_declaration : type variable_declarators
+                                  | variable_modifiers type variable_declarators'''
+    if len(p) == 2:
+        p[0] = ast.one_child_node("local_variable_declaration", p[1])
+    else:
+        p[0] = ast.three_child_node("local_variable_declaration", p[1], p[2], p[3])
+
+def p_variable_modfiers(p):
+    '''variable_modifiers : variable_modifier
+                          | variable_modifiers variable_modifier'''
+    if len(p) == 2:
+        p[0] = ast.one_child_node("variable_modifiers", p[1])
+    else:
+        p[0] = ast.two_child_node("variable_modifiers", p[1], p[2])
+
+def p_variable_modifier(p):
+    '''variable_modifier : FINAL'''
+    tmp = ast.node_create(p[1])
+    p[0] = ast.one_child_node("variable_modifier", tmp)
+
+def p_statement_rule1(p):
+    '''statement : block
+                 | expression SEMICOLON'''
+    if len(p) == 2:
+        p[0] = ast.one_child_node("statement", p[1])
+    else:
+        tmp = ast.node_create(p[2])
+        p[0] = ast.two_child_node("statement", p[1], tmp)
+
+def p_statement_rule2(p):
+    '''statement : RETURN
+                 | RETURN expression'''
+    if len(p) == 2:
+        p[0] = ast.one_child_node("statement", p[1])
+    else:
+        tmp = ast.node_create(p[1])
+        p[0] = ast.three_child_node("statement", tmp, p[2])
+
+def p_variable_declarators(p):
+    '''variable_declarators : variable_declarator
+                            | variable_declarators COMMA variable_declarator'''
+    if len(p) == 2:
+        p[0] = ast.one_child_node("variable_declarators", p[2])
+    else:
+        tmp = ast.node_create(p[1])
+        p[0] = ast.three_child_node("variable_declarators", p[1], tmp, p[3])
+
+def p_variable_declarator(p):
+    '''variable_declarator : variable_declarator_id
+                           | variable_declarator_id EQ variable_initializer'''
+    if len(p) == 2:
+        p[0] = ast.one_child_node("variable_declarator", p[2])
+    else:
+        tmp = ast.node_create(p[1])
+        p[0] = ast.three_child_node("variable_declarator", p[1], tmp, p[3])
+
+def p_variable_declarator_id(p):
+    '''variable_declarator_id : IDENTIFIER'''
+    tmp = ast.node_create(p[1])
+    p[0] = ast.one_child_node("variable_declarator_id", tmp)
+
+def p_variable_initializer(p):
+    '''variable_initializer : expression'''
+    p[0] = ast.one_child_node("variable_initializer", p[1])
+
+def p_type(p):
+    '''type : primitive_type'''
+    p[0] = ast.one_child_node("type", p[1])
+
+def p_primitive_type(p):
+    '''primitive_type : BOOLEAN
+                      | VOID
+                      | SHORT
+                      | INT
+                      | LONG
+                      | CHAR
+                      | FLOAT
+                      | DOUBLE'''
+    tmp = ast.node_create(p[1])
+    p[0] = ast.one_child_node("primitive_type", tmp)
+
+# Expressions
 def p_expression(p):
     '''expression : logical_expression
                   | logical_expression assignment_operator expression'''
     if len(p) == 2:
-        p[0] = ast.unary_op("expression", p[1])
+        p[0] = ast.one_child_node("expression", p[1])
     else:
-        p[0] =  ast.binary_op(p[2], p[1], p[3])
-    ast.end()
+        p[0] =  ast.three_child_node("expression", p[1], p[2], p[3])
 
 def p_constant_expression(p):
     '''constant_expression : expression'''
-    p[0] = ast.unary_op("constant_expression", tmp)
-
+    p[0] = ast.one_child_node("constant_expression", tmp)
 
 def p_assignment_operator(p):
     '''assignment_operator : EQ
@@ -30,11 +137,14 @@ def p_assignment_operator(p):
                            | AND_ASSIGN
                            | OR_ASSIGN
                            | XOR_ASSIGN'''
-    p[0] = p[1]
+    tmp = ast.node_create(p[1])
+    p[0] = ast.one_child_node("assignment_operator", tmp)
 
 def p_par_expression(p):
     '''par_expression : LPAREN expression RPAREN'''
-    p[0] = p[2]
+    lparen = ast.node_create(p[1])
+    rparen = ast.node_create(p[3])
+    p[0] = ast.three_child_node("par_expression", lparen, p[2], rparen)
 
 # Ternary expression grammar not implemented
 
@@ -43,9 +153,10 @@ def p_logical_expression(p):
                           | logical_expression AND bitwise_expression
                           | logical_expression OR bitwise_expression'''
     if len(p) == 2:
-        p[0] = ast.unary_op("bitwise_expression", p[1])
+        p[0] = ast.one_child_node("logical_expression", p[1])
     else:
-        p[0] = ast.binary_op(p[2], p[1], p[3])
+        tmp = ast.node_create(p[2])
+        p[0] = ast.three_child_node("logical_expression", p[1], tmp, p[3])
 
 def p_bitwise_expression(p):
     '''bitwise_expression : equality_expression
@@ -53,20 +164,20 @@ def p_bitwise_expression(p):
                           | bitwise_expression BIT_OR equality_expression
                           | bitwise_expression XOR equality_expression'''
     if len(p) == 2:
-        p[0] = ast.unary_op("bitwise_expression", p[1])
+        p[0] = ast.one_child_node("bitwise_expression", p[1])
     else:
-        p[0] = ast.binary_op(p[2], p[1], p[3])
-
+        tmp = ast.node_create(p[2])
+        p[0] = ast.three_child_node("bitwise_expression", p[1], tmp, p[3])
 
 def p_equality_expression(p):
     '''equality_expression : comparision_expression
                            | equality_expression EQUALITY comparision_expression
                            | equality_expression NE comparision_expression'''
     if len(p) == 2:
-        p[0] = ast.unary_op("equality_expression", p[1])
+        p[0] = ast.one_child_node("equality_expression", p[1])
     else:
-        p[0] = ast.binary_op(p[2], p[1], p[3])
-
+        tmp = ast.node_create(p[2])
+        p[0] = ast.three_child_node("equality_expression", p[1], tmp, p[3])
 
 def p_comparision_expression(p):
     '''comparision_expression : shift_expression
@@ -75,9 +186,10 @@ def p_comparision_expression(p):
                               | comparision_expression LT shift_expression
                               | comparision_expression GT shift_expression'''
     if len(p) == 2:
-        p[0] = ast.unary_op("comparison_expression", p[1])
+        p[0] = ast.one_child_node("comparison_expression", p[1])
     else:
-        p[0] = ast.binary_op(p[2], p[1], p[3])
+        tmp = ast.node_create(p[2])
+        p[0] = ast.three_child_node("comparison_expression", p[1], tmp, p[3])
 
 def p_shift_expression(p):
     '''shift_expression : additive_expression
@@ -85,9 +197,10 @@ def p_shift_expression(p):
                         | shift_expression RSHIFT additive_expression
                         | shift_expression RRSHIFT additive_expression'''
     if len(p) == 2:
-        p[0] = ast.unary_op("shift_expression", p[1])
+        p[0] = ast.one_child_node("shift_expression", p[1])
     else:
-        p[0] = ast.binary_op(p[2], p[1], p[3])
+        tmp = ast.node_create(p[2])
+        p[0] = ast.three_child_node("shift_expression", p[1], tmp, p[3])
 
 def p_additive_expression(p):
     '''additive_expression : multiplicative_expression
@@ -95,9 +208,10 @@ def p_additive_expression(p):
                            | additive_expression MINUS multiplicative_expression'''
 
     if len(p) == 2:
-        p[0] = ast.unary_op("additive_expression", p[1])
+        p[0] = ast.one_child_node("additive_expression", p[1])
     else:
-        p[0] = ast.binary_op(p[2], p[1], p[3])
+        tmp = ast.node_create(p[2])
+        p[0] = ast.three_child_node("additive_expression", p[1], tmp, p[3])
 
 def p_multiplicative_expression(p):
     '''multiplicative_expression : unary_expression
@@ -105,34 +219,35 @@ def p_multiplicative_expression(p):
                                  | multiplicative_expression BY unary_expression'''
 
     if len(p) == 2:
-        p[0] = ast.unary_op("multiplicative_expression", p[1])
+        p[0] = ast.one_child_node("multiplicative_expression", p[1])
     else:
-        p[0] = ast.binary_op(p[2], p[1], p[3])
+        tmp = ast.node_create(p[2])
+        p[0] = ast.three_child_node("multiplicative_expression", p[1], tmp, p[3])
 
 def p_unary_expression(p):
     '''unary_expression : PLUS unary_expression
                         | MINUS unary_expression
                         | primary'''
     if len(p) == 2:
-        p[0] = ast.unary_op("unary_expression", p[1])
+        p[0] = ast.one_child_node("unary_expression", p[1])
     else:
-        tmp = ast.unary_op(p[1], p[2])
-        p[0] = ast.unary_op("unary_expression", tmp)
+        tmp = ast.node_create(p[1])
+        p[0] = ast.two_child_node("unary_expression", tmp, p[2])
 
 def p_primary(p):
     '''primary : literal
                | par_expression'''
-    p[0] = ast.unary_op("primary", p[1])
+    p[0] = ast.one_child_node("primary", p[1])
 
 def p_primary_identifier(p):
     '''primary : IDENTIFIER'''
-    tmp = ast.single_node(p[1])
-    p[0] = ast.unary_op("primary", tmp)
+    tmp = ast.node_create(p[1])
+    p[0] = ast.one_child_node("primary", tmp)
 
 def p_literal(p):
     '''literal : NUMBER'''
-    tmp = ast.single_node(p[1])
-    p[0] = ast.unary_op("literal", tmp)
+    tmp = ast.node_create(p[1])
+    p[0] = ast.one_child_node("literal", tmp)
 
 # Error rule for syntax errors
 def p_error(p):
@@ -147,4 +262,4 @@ while True:
    except EOFError:
        break
    if not s: continue
-   result = parser.parse(s)
+   result = parser.parse(s, debug=1)
