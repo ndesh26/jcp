36077ef2f86a5b87ab19a82e45b15225a06bf7ec
Nayan Deshmukh <nayan26deshmukh@gmail.com>
2017-02-12 20:08:09 +0530

    use plyj grammar
    Signed-off-by: Nayan Deshmukh <nayan26deshmukh@gmail.com>

--- jcp/lexer.py
+++ jcp/lexer.py
@@ -7,122 +7,81 @@
 import ply.lex as lex
 
 # Reference: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/_keywords.html
-keywords = ('abstract', 'assert', 'boolean', 'break', 'byte',
-            'case', 'catch', 'char', 'class', 'const', 'continue',
-            'default', 'do', 'double', 'else', 'enum', 'extends',
-            'final', 'finally', 'float', 'for', 'goto', 'if',
-            'implements', 'import', 'instanceof', 'int', 'interface',
-            'long', 'native', 'new', 'package', 'private', 'protected',
-            'public', 'return', 'short', 'static', 'strictfp', 'super',
-            'switch', 'synchronized', 'this', 'throw', 'throws',
-            'transient', 'try', 'void', 'volatile', 'while')
+keywords = ('this', 'class', 'void', 'super', 'extends', 'implements', 'enum', 'interface',
+            'byte', 'short', 'int', 'long', 'char', 'float', 'double', 'boolean', 'null',
+            'true', 'false',
+            'final', 'public', 'protected', 'private', 'abstract', 'static', 'strictfp', 'transient', 'volatile',
+            'synchronized', 'native',
+            'throws', 'default',
+            'instanceof',
+            'if', 'else', 'while', 'for', 'switch', 'case', 'assert', 'do',
+            'break', 'continue', 'return', 'throw', 'try', 'catch', 'finally', 'new',
+            'package', 'import'
+            )
 
-# Reference: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/operators.html
-# Tokens definitions
 tokens = [
-        'IDENTIFIER',
-        'NUMBER',
-        'CHARACTER',
-        'STRING',
+        'NAME',
+        'NUM',
+        'CHAR_LITERAL',
+        'STRING_LITERAL',
         'LINE_COMMENT', 'BLOCK_COMMENT',
 
-        'OR', 'AND', 'BIT_OR', 'BIT_AND',
-        'EQ', 'NE', 'GE', 'LE',
-
-        'PLUS', 'MINUS', 'LPAREN', 'RPAREN',
-        'TIMES', 'BY', 'GT', 'LT', 'REMAINDER',
-        'RBRACE', 'LBRACE','LSQRBRACKET', 'RSQRBRACKET',
-
+        'OR', 'AND',
+        'EQ', 'NEQ', 'GTEQ', 'LTEQ',
         'LSHIFT', 'RSHIFT', 'RRSHIFT',
 
-        'INCR', 'DECR',
-
-        'EQUALITY', 'NEQUALITY',
-
-        'PLUS_ASSIGN', 'MINUS_ASSIGN', 'TIMES_ASSIGN','BY_ASSIGN',
-        'REMAINDER_ASSIGN', 'LSHIFT_ASSIGN', 'RSHIFT_ASSIGN', 'RRSHIFT_ASSIGN',
+        'TIMES_ASSIGN', 'DIVIDE_ASSIGN', 'REMAINDER_ASSIGN',
+        'PLUS_ASSIGN', 'MINUS_ASSIGN', 'LSHIFT_ASSIGN', 'RSHIFT_ASSIGN', 'RRSHIFT_ASSIGN',
         'AND_ASSIGN', 'OR_ASSIGN', 'XOR_ASSIGN',
 
-        'TERNARY', 'XOR', 'SEMICOLON', 'COMMA', 'DOT',
-        'QUESTION', 'COLON'
-        ] + [k.upper() for k in keywords]
+        'PLUSPLUS', 'MINUSMINUS',
 
-t_NUMBER        = r'\d+'
-t_CHARACTER     = r'\'([^\\\n]|(\\.))*?\''
-t_STRING        = r'\"([^\\\n]|(\\.))*?\"'
-t_LINE_COMMENT  = '//.*'
+        'ELLIPSIS'
+] + [k.upper() for k in keywords]
+literals = '()+-*/=?:,.^|&~!=[]{};<>@%'
+
+t_NUM = r'\.?[0-9][0-9eE_lLdDa-fA-F.xXpP]*'
+t_CHAR_LITERAL = r'\'([^\\\n]|(\\.))*?\''
+t_STRING_LITERAL = r'\"([^\\\n]|(\\.))*?\"'
+
+t_ignore_LINE_COMMENT = '//.*'
 
 def t_BLOCK_COMMENT(t):
     r'/\*(.|\n)*?\*/'
     t.lexer.lineno += t.value.count('\n')
 
-# LOGICAL and BITWISE OPERATIONS
-t_OR        = r'\|\|'
-t_AND       = r'&&'
-t_BIT_OR    = r'\|'
-t_BIT_AND   = r'&'
-t_XOR       = r'\^'
+t_OR = r'\|\|'
+t_AND = '&&'
 
-# COMPARISIONS
-t_GT    = '>'
-t_LT    = '<'
-t_EQ    = '='
-t_NE    = '!='
-t_GE    = '>='
-t_LE    = '<='
+t_EQ = '=='
+t_NEQ = '!='
+t_GTEQ = '>='
+t_LTEQ = '<='
 
-# ARITHEMETIC OPERATIONS
-t_PLUS      = r'\+'
-t_MINUS     = r'\-'
-t_TIMES     = r'\*'
-t_BY        = r'/'
-t_REMAINDER = '%'
+t_LSHIFT = '<<'
+t_RSHIFT = '>>'
+t_RRSHIFT = '>>>'
 
-# SHIFT
-t_LSHIFT    = '<<'
-t_RSHIFT    = '>>'
-t_RRSHIFT   = '>>>'
+t_TIMES_ASSIGN = r'\*='
+t_DIVIDE_ASSIGN = '/='
+t_REMAINDER_ASSIGN = '%='
+t_PLUS_ASSIGN = r'\+='
+t_MINUS_ASSIGN = '-='
+t_LSHIFT_ASSIGN = '<<='
+t_RSHIFT_ASSIGN = '>>='
+t_RRSHIFT_ASSIGN = '>>>='
+t_AND_ASSIGN = '&='
+t_OR_ASSIGN = r'\|='
+t_XOR_ASSIGN = '\^='
 
-# INCREMENT & DECREMENT
-t_INCR      = r'\+\+'
-t_DECR      = r'\-\-'
+t_PLUSPLUS = r'\+\+'
+t_MINUSMINUS = r'\-\-'
 
-# EQUALITY
-t_EQUALITY  = '=='
+t_ELLIPSIS = r'\.\.\.'
 
-# ASSIGNMENT
-t_PLUS_ASSIGN       = r'\+='
-t_MINUS_ASSIGN      = '-='
-t_TIMES_ASSIGN      = r'\*='
-t_BY_ASSIGN         = '/='
-t_REMAINDER_ASSIGN  = '%='
-t_LSHIFT_ASSIGN     = '<<='
-t_RSHIFT_ASSIGN     = '>>='
-t_RRSHIFT_ASSIGN    = '>>>='
-t_AND_ASSIGN        = '&='
-t_OR_ASSIGN         = r'\|='
-t_XOR_ASSIGN        = '\^='
+t_ignore = ' \t\f'
 
-# BRACKETS
-t_LPAREN     = r'\('
-t_RPAREN     = r'\)'
-t_LBRACE     = r'\{'
-t_RBRACE     = r'\}'
-t_LSQRBRACKET= r'\['
-t_RSQRBRACKET= r'\]'
-
-# DELIMITER
-t_SEMICOLON = r';'
-t_COMMA     = r','
-t_DOT       = r'\.'
-t_QUESTION  = r'\?';
-t_COLON     = r':';
-
-# Ignore indentation
-t_ignore    = ' \t'
-
-# VARIABLES & NEWLINES
-def t_IDENTIFIER(t):
+def t_NAME(t):
     '[A-Za-z_$][A-Za-z0-9_$]*'
     if t.value in keywords:
         t.type = t.value.upper()
@@ -133,8 +92,8 @@ def t_newline(t):
     t.lexer.lineno += len(t.value)
 
 def t_newline2(t):
-    r'\r\n'
-    t.lexer.lineno += len(t.value)/2
+    r'(\r\n)+'
+    t.lexer.lineno += len(t.value) / 2
 
 def t_error(t):
     print("Illegal character '{}' ({}) in line {}".format(t.value[0], hex(ord(t.value[0])), t.lexer.lineno))
