b9e05d198f76c9adffdd83152225fdd600eb19fb
Nayan Deshmukh <nayan26deshmukh@gmail.com>
2017-02-12 23:47:10 +0530

    parser: remove prefix and add a debug flag
    Signed-off-by: Nayan Deshmukh <nayan26deshmukh@gmail.com>

--- jcp/parser.py
+++ jcp/parser.py
@@ -3,25 +3,24 @@ import ply.yacc as yacc
 from parser_rules import JavaParser
 import ptg
 
-if len(sys.argv) != 3:
-    print("Usage: {} parser filename".format(sys.argv[0]))
+if len(sys.argv) < 2:
+    print("Usage: {} [-g] filename".format(sys.argv[0]))
     exit()
     
+if sys.argv[1] == "-g":
+    debug = 1
+else:
+    debug = 0
 # Build the parser
 parser = yacc.yacc(module=JavaParser(), start='goal')
-if type(sys.argv[2]) == str:
-    _file = open(sys.argv[2])
-content = _file.read()
-
-if sys.argv[1] == 'expression':
-    prefix = '--'
-elif sys.argv[1] == 'program':
-    prefix = '++'
-elif sys.argv[1] == 'statement':
-    prefix = '**'
+if len(sys.argv) == 3:
+    if type(sys.argv[2]) == str:
+        _file = open(sys.argv[2])
+    content = _file.read()
 else:
-    print("Parser not found")
-    exit()
-   
-result = parser.parse(prefix+content, debug=1)
+    if type(sys.argv[1]) == str:
+        _file = open(sys.argv[1])
+    content = _file.read()
+
+result = parser.parse("++"+content, debug=debug)
 ptg.end()
