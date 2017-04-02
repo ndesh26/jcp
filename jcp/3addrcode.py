71b8d563e345a680864451e07dffaabc1ed68236
Rohith Mukku <rohithmukku@gmail.com>
2017-04-02 15:07:32 +0530

    3addrcode: add code TAC for IfStmt
    Signed-off-by: Rohith Mukku <rohithmukku@gmail.com>

--- jcp/3addrcode.py
+++ jcp/3addrcode.py
@@ -13,7 +13,10 @@ class BinOp(Ins):
         self.dst = dst
 
     def __repr__(self):
-        return self.dst['value'] + ' = ' + '{}'.format(self.arg1['value']) + ' ' + self.op + ' ' + '{}'.format(self.arg2['value'])
+        if self.label == "":
+            return self.dst['value'] + ' = ' + '{}'.format(self.arg1['value']) + ' ' + self.op + ' ' + '{}'.format(self.arg2['value'])
+        else:
+            return self.label + ':' + self.dst['value'] + ' = ' + '{}'.format(self.arg1['value']) + ' ' + self.op + ' ' + '{}'.format(self.arg2['value'])
 
 class AssignOp(Ins):
     def __init__(self, label="", op="", arg=None, dst=None):
@@ -22,7 +25,26 @@ class AssignOp(Ins):
         self.dst = dst
 
     def __repr__(self):
-        return self.dst['value'] + ' = ' + '{}'.format(self.arg['value'])
+        if self.label == "":
+            return self.dst['value'] + ' = ' + '{}'.format(self.arg['value'])
+        else:
+            return self.label + ':' + self.dst['value'] + ' = ' + '{}'.format(self.arg['value'])
+
+class IfStmt(Ins):
+    def __init__(self, label="", op="", arg=None, target=""):
+        Ins.__init__(self, label, op)
+        self.arg = arg
+        self.target = target
+
+    def __repr__(self):
+        return 'IfZ ' + '{}'.format(self.arg['value']) + ' Goto ' + self.target
+
+class Label(Ins):
+    def __init__(self, label=""):
+        Ins.__init__(self, label)
+
+    def __repr__(self):
+        return self.label
 
 class Tac(object):
     def __init__(self):
@@ -42,6 +64,21 @@ class Tac(object):
                 binop = BinOp(op=node.value, arg1=arg1, arg2=arg2, dst=dst)
                 self.code.append(binop)
                 return dst
+
+        elif node.name == "IfStmt":
+            arg1 = self.generate_tac(node.children[0])
+            iflbl = symbol_table.get_target()
+            ifop = IfStmt(arg=arg1, target=iflbl)
+            self.code.append(ifop)
+            arg2 = self.generate_tac(node.children[2])
+            afterlbl = symbol_table.get_target()
+            afterop = Label(label="Goto"+afterlbl)
+            self.code.append(afterop)
+            ifop = Label(label=iflbl+":")
+            self.code.append(ifop)
+            arg3 = self.generate_tac(node.children[1])
+            afterop = Label(label=afterlbl+":")
+            self.code.append(afterop)
 
         elif node.name == "IntegerLiteral":
             return {'value': node.value, 'type': "int"}
