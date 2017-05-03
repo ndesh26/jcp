10f805e1cb89ccb4a1de8cc1f153fbb67ba6d819
Nayan Deshmukh <nayan26deshmukh@gmail.com>
2017-05-03 23:33:06 +0530

    test: add extra test case for switch case
    Signed-off-by: Nayan Deshmukh <nayan26deshmukh@gmail.com>

---
+++ test/extra/switch_case.java
@@ -0,0 +1,16 @@
+class main {
+    public void main() {
+        int i = 5;
+
+        switch(i) {
+            case 1:
+                printString("wrong");
+                break;
+            case 5:
+                printString("correct");
+                break;
+            default:
+                printString("wrong");
+        }
+    }
+}
